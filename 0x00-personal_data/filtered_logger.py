ccb2527324f951aa92a5ccc99f800f650e2f46f9
fardosa001 <fardosaadow@gmail.com>
2024-04-11 16:54:09 +0000

    regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,8 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated"""
-    return re.sub(r'({})[^{}]+'.format('|'.join(fields), separator),
-                  r'\1{}'.format(redaction), message)
+    pattern = r'({})[^{}]+'.format('|'.join(fields), separator)
+    return re.sub(pattern, r'\1{}'.format(redaction), message)
