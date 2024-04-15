25bac1b55ee97da22baf1536a403d44e743b2a11
fardosa001 <fardosaadow@gmail.com>
2024-04-15 17:43:09 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,15 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized():
+    """ GET /api/v1/unauthorized
+    Raises:
+        401: Unauthorized
+    """
+    abort(401)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
