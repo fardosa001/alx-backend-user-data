0728ad195d5080b05f45e078967520583207e5cf
fardosa001 <fardosaadow@gmail.com>
2024-04-16 20:29:02 +0000

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,12 +58,10 @@ class BasicAuth(Auth):
 
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        # Search for the user in the database
-        user = User.search({'email': user_email})
-
-        if user is None:
+        try:
+            users = User.search({'email': user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
             return None
-        if not user.is_valid_password(user_pwd):
-            return None
-
-        return user
