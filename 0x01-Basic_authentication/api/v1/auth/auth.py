6240719804da1770699af9c5573973dff1b31814
fardosa001 <fardosaadow@gmail.com>
2024-04-16 17:43:38 +0000

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """class to manage the API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if authentication is required for the given path."""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Get the authorization header from the request"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Get the current user based on the request"""
+        return None
