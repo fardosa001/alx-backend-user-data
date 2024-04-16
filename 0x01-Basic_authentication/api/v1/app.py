dac6998974a9cba97d41111f4de8e12a3e21a58f
fardosa001 <fardosaadow@gmail.com>
2024-04-16 18:22:28 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,12 +15,11 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE == 'auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-elif AUTH_TYPE == 'basic_auth':
+if AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+else:
+    auth = Auth()
 
 
 @app.before_request
