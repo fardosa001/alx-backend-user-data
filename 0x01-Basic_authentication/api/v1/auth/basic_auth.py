946bff692416ec2d0662de06566a800ecb3946d0
fardosa001 <fardosaadow@gmail.com>
2024-04-16 20:20:17 +0000

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,17 @@ class BasicAuth(Auth):
             return decoded_string
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """returns the user email and password
+        from the Base64 decoded value."""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        user_cred = decoded_base64_authorization_header.split(':', 1)
+        return user_cred[0], user_cred[1]
