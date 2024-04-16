2d59e585c8f94b4c4d8bf4f6f307ca0d650806ec
fardosa001 <fardosaadow@gmail.com>
2024-04-16 17:21:42 +0000

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,7 +21,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
+@app.errorhandler(403)
 def unauthorized(error):
     """ forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
