6f86772500ecdd8a526779f7a010081ed6b35d73
fardosa001 <fardosaadow@gmail.com>
2024-04-20 15:34:49 +0000

    implement Basic authentication for giving you access to all User endpoint

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -25,6 +25,12 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+
+    if user_id == 'me' and request.current_user is None:
+        return jsonify(request.current_user.to_json()), 200
+
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -44,17 +50,11 @@ def delete_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-
-    if user_id == 'me':
-        if request.current_user is None:
-            abort(404)
-        else:
-            return jsonify(request.current_user.to_json()), 200
-
     user = User.get(user_id)
     if user is None:
         abort(404)
-    return jsonify(user.to_json())
+    user.remove()
+    return jsonify({}), 200
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
