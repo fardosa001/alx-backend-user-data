639ea6148b40dc03026b3efdc26fe6b0d99b29e2
fardosa001 <fardosaadow@gmail.com>
2024-04-21 12:08:05 +0000

    fix task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,8 +38,7 @@ def before_request():
                       '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if not auth.authorization_header(request)\
-       and not auth.session_cookie(request):
+    if not auth.authorization_header(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
