5e54174749a5f42a5460aa240e711f4e4e6b2bfb
fardosa001 <fardosaadow@gmail.com>
2024-04-23 17:13:22 +0000

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,5 +35,5 @@ class DB:
 
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
-        self.__session.commit()
+        self._session.commit()
         return new_user
