ea3a5336a55d59d1e91b86dd5e59f50eecb694a7
fardosa001 <fardosaadow@gmail.com>
2024-04-23 17:37:22 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """Find a user in the database by arbitrary keyword arguments"""
         if kwargs is None:
             raise InvalidRequestError
