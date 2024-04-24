f276e9f42c35aebaf46ff416d53e6aa380cd0857
fardosa001 <fardosaadow@gmail.com>
2024-04-24 15:24:23 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,9 +32,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hash_password: str) -> User:
         """ save the user to the database and returns a User object """
-        user = User(email=email, hashed_password=hashed_password)
+        user = User(email=email, hash_password=hash_password)
         self._session.add(user)
         self._session.commit()
         return user
