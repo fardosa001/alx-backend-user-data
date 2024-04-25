a214a6d861beae830547d037e86f4a4c649c8bb2
fardosa001 <fardosaadow@gmail.com>
2024-04-25 11:41:54 +0000

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Hash password"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """ hash password method"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
