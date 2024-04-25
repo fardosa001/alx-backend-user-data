f731c55b933b11901fe7f411c80c6680fcea1bc8
fardosa001 <fardosaadow@gmail.com>
2024-04-25 18:36:29 +0000

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,7 +77,7 @@ def get_reset_password_token():
 
     reset_token = AUTH.get_reset_password_token(email)
 
-    return jsonify({"email": email, "reset_token": token}), 200
+    return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
 if __name__ == "__main__":
