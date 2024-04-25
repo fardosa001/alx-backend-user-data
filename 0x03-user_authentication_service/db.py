d17e4faef154314d1c7c19f7f71377a6e762afd5
fardosa001 <fardosaadow@gmail.com>
2024-04-25 11:14:48 +0000

    update  user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,15 +34,15 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ save the user to the database and returns a User object """
-        new_user = User(email=email, hashed_password=hashed_password)
+        user = User(email=email, hashed_password=hashed_password)
         try:
-            self._session.add(new_user)
+            self._session.add(user)
             self._session.commit()
-        except Exception as e:
-            print(f"Error adding user to database: {e}")
+            return user
+        except Exception:
             self._session.rollback()
-            raise
-        return new_user
+            user = None
+            return user
 
     def find_user_by(self, **kwargs) -> User:
         """Find a user in the database by arbitrary keyword arguments"""
